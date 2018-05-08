fead3294af2c9457f9109039d210cec0202265ea
danteidbratt <32329674+danteidbratt@users.noreply.github.com>
2018-05-08 10:49:25 +0200

    Task, Status

--- src/main/java/se/steam/trellov2/model/Task.java
+++ src/main/java/se/steam/trellov2/model/Task.java
@@ -1,12 +1,35 @@
 package se.steam.trellov2.model;
 
+import java.util.UUID;
+
 public final class Task extends AbstractDomainModel<Task> {
 
     private final String text;
+    private Status status;
 
+    public Task(String text, Status status) {
+        super(null);
+        this.text = text;
+        this.status = status;
+    }
+
+    public Task(UUID id, String text, Status status) {
+        super(id);
+        this.text = text;
+        this.status = status;
+    }
 
     @Override
     public Task assignId() {
-        return null;
+        return new Task(UUID.randomUUID(), text, status);
     }
+
+    public String getText() {
+        return text;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
 }
