f7912aec88522ff0105d753121db6681f169980f
danteidbratt <32329674+danteidbratt@users.noreply.github.com>
2018-05-08 10:30:52 +0200

    lol

---
+++ src/main/java/se/steam/trellov2/model/Task.java
@@ -0,0 +1,12 @@
+package se.steam.trellov2.model;
+
+public final class Task extends AbstractDomainModel<Task> {
+
+    private final String text;
+
+
+    @Override
+    public Task assignId() {
+        return null;
+    }
+}
--- src/main/java/se/steam/trellov2/service/UserService.java
+++ src/main/java/se/steam/trellov2/service/UserService.java
@@ -1,5 +1,10 @@
 package se.steam.trellov2.service;
 
+import se.steam.trellov2.model.User;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface UserService {
 
     User get(UUID id);
