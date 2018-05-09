8c771a0ad299f764cfc728b2248bbd9fad5bab58
frfr1701 <fredrik.frolund@yh.nackademin.se>
2018-05-09 12:57:07 +0200

    la till UserResource. Sedan flytta issueStatus till en status map.

--- src/main/java/se/steam/trellov2/resource/UserResource.java
+++ src/main/java/se/steam/trellov2/resource/UserResource.java
@@ -3,6 +3,8 @@ package se.steam.trellov2.resource;
 import org.springframework.stereotype.Component;
 import se.steam.trellov2.model.Task;
 import se.steam.trellov2.model.User;
+import se.steam.trellov2.resource.parameter.UserInput;
+import se.steam.trellov2.service.TaskService;
 import se.steam.trellov2.service.UserService;
 
 import javax.ws.rs.*;
@@ -20,43 +22,54 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Produces(APPLICATION_JSON)
 public final class UserResource {
 
-    private final UserService service;
+    private final UserService uService;
+    private final TaskService tService;
 
     @Context
     private UriInfo uriInfo;
 
-    private UserResource(UserService service) {
-        this.service = service;
+    private UserResource(UserService uService, TaskService tService) {
+        this.uService = uService;
+        this.tService = tService;
     }
 
     @GET
-    public List<User> getUsers(){
-        return service.getAll();
+    public List<User> getUsers(@BeanParam UserInput input) {
+        return uService.getAll(input);
     }
 
     @GET
     @Path("{id}")
-    public User getUser(@PathParam("id") UUID id){
-        return service.get(id);
+    public User getUser(@PathParam("id") UUID id) {
+        return uService.get(id);
     }
 
     @PUT
     @Path("{id}")
-    public void updateUser(@PathParam("id") UUID id, User user){
-        service.update(new User(id,user.getUsername(),user.getFirstName(),user.getLastName(),user.isActive()));
+    public void updateUser(@PathParam("id") UUID id, User user) {
+        uService.update(new User(id, user.getUsername(), user.getFirstName(), user.getLastName(), user.isActive()));
     }
 
     @POST
-    public Response postUser(User user){
-        return Response.created(uriInfo.getAbsolutePathBuilder().path(service.save(user).getId().toString()).build()).build();
+    public Response postUser(User user) {
+        return Response.created(uriInfo.getAbsolutePathBuilder().path(uService.save(user).getId().toString()).build()).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void removeUser(@PathParam("id") UUID id){
+        uService.remove(id);
     }
 
     @GET
     @Path("{id}/tasks")
-    public List<Task> getTasksByUser(@PathParam("id") UUID id){
-        return null;
+    public List<Task> getTasksByUser(@PathParam("id") UUID id) {
+        return tService.getByUser(id);
     }
 
-
-
+    @PUT
+    @Path("{id}/tasks/{taskId}")
+    public void addTaskToUser(@PathParam("id") UUID id, @PathParam("taskId") UUID taskId) {
+        uService.addTask(id, taskId);
+    }
 }
