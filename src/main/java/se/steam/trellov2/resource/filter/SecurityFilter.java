fb3a924bb55e231373b35cdb680c8f85858857c4
Santos92 <AlexanderLundbergSantos@live.se>
2018-05-14 20:16:14 +0200

    ändrat till environment i securityFilter

--- src/main/java/se/steam/trellov2/resource/filter/SecurityFilter.java
+++ src/main/java/se/steam/trellov2/resource/filter/SecurityFilter.java
@@ -1,6 +1,10 @@
 package se.steam.trellov2.resource.filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import se.steam.trellov2.resource.mapper.Secured;
 
@@ -8,27 +12,31 @@ import javax.annotation.Priority;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
 
 import static java.util.Collections.singletonMap;
 import static javax.ws.rs.Priorities.*;
 import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
 import static javax.ws.rs.core.Response.status;
 
-@Secured
 @Component
+@Secured
 @Provider
 @Priority(AUTHENTICATION)
-public class SecurityFilter implements ContainerRequestFilter {
+public final class SecurityFilter implements ContainerRequestFilter {
 
-    @Value("authtoken") String key;
+    private Environment environment;
+    public SecurityFilter(Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
-    public void filter(ContainerRequestContext request) throws IOException {
-
-        if (key.equalsIgnoreCase(request.getHeaderString("key")))
+    public void filter(ContainerRequestContext request) {
+        System.out.println(environment.getProperty("trellov2.authtoken"));
+        System.out.println(request.getHeaderString("key"));
+        if (!environment.getProperty("trellov2.authtoken").equalsIgnoreCase(request.getHeaderString("key")))
             request.abortWith(status(UNAUTHORIZED)
                     .entity(singletonMap("Error 401", "Missing/Invalid api key"))
                     .build());
     }
 }
+
