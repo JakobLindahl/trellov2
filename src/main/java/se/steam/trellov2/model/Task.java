42532da90261eb3b883b81dd852bd503644fe668
frfr1701 <fredrik.frolund@yh.nackademin.se>
2018-05-14 11:53:00 +0200

    La till remove och delete i resources och la till updateabe=false på team columnen i Task entiteten

--- src/main/java/se/steam/trellov2/model/Task.java
+++ src/main/java/se/steam/trellov2/model/Task.java
@@ -1,8 +1,10 @@
 package se.steam.trellov2.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import org.apache.tomcat.jni.Local;
 import se.steam.trellov2.model.status.TaskStatus;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 import static se.steam.trellov2.model.status.TaskStatus.UNSTARTED;
@@ -11,23 +13,26 @@ public final class Task extends AbstractModel<Task> {
 
     private final String text;
     private final TaskStatus status;
+    private final LocalDate date;
 
     @JsonCreator
     public Task(String text) {
         super(null);
         this.text = text;
         this.status = UNSTARTED;
+        this.date = LocalDate.now();
     }
 
-    public Task(UUID id, String text, TaskStatus status) {
+    public Task(UUID id, String text, TaskStatus status, LocalDate date) {
         super(id);
         this.text = text;
         this.status = status;
+        this.date = date;
     }
 
     @Override
     public Task assignId() {
-        return new Task(UUID.randomUUID(), text, status);
+        return new Task(UUID.randomUUID(), text, status, date);
     }
 
     public String getText() {
@@ -38,7 +43,11 @@ public final class Task extends AbstractModel<Task> {
         return status;
     }
 
+    public LocalDate getDate() {
+        return date;
+    }
+
     public Task setStatus(TaskStatus status) {
-        return new Task(getId(), text, status);
+        return new Task(getId(), text, status, LocalDate.now());
     }
 }
